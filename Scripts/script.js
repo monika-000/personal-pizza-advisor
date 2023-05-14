f1dbbd3c5cfd3ba1ac971ec85a1c4b1f8c0cebd2
Monika <mprzybylska601@gmail.com>
2023-05-15 08:59:59 +1000

    Updated JS and HTML

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,42 +7,39 @@
 * @author Monika Przybylska
 */
 //function checkNumberOfPeople(){
-
-	let bestPrice, optionThree, reminderThree, optionTwo, reminderTwo, optionOne ;
-	const onePizzaPriceSmall = 6.50;
-	const onePizzaPriceLarge = 8.50;
-	const twoPizzasPriceSmall = 9;
-	const twoPizzasPriceLarge = 11;
-	const threePizzasPriceSmall = 12;
-	const threePizzasPriceLarge = 14;
-	let numOfPizzas= document.getElementById("quantity").value;
+	let recommendedNumber;
 	let calcBtn = document.getElementById("calcBtn");
 	let radioBtn = document.getElementsByName("hungryValue");
-	//radioBtn = false;
 	
 	calcBtn.addEventListener('click', ()=>{
 		let  radioBtnValue;
-		debugger;
+		let numOfPizzas= document.getElementById("quantity").value;
 		for(let i = 0; i< radioBtn.length; i++){
 			if(radioBtn[i].checked){
-				radioBtnValue = radioBtn[i].value;
+				radioBtnValue = radioBtn[i].labels[0].innerText;	
 			}
 		}
-		debugger;
 		if (numOfPizzas == "" || isNaN(numOfPizzas) || numOfPizzas %  1 != 0 || numOfPizzas <= 0 || radioBtnValue == "undefined"){
 			document.getElementById("message").textContent = `Please provide a number and choose how hungry you are`;
 		}
 		else{
-			console.log("Hi")
-			// //Calculate the best option for entered number of pizzas
-			// 	optionThree = Math.floor(numOfPizzas / 3);
-			// 	reminderThree = numOfPizzas % 3;
-			// 	optionTwo = Math.floor(reminderThree / 2);
-			// 	reminderTwo = reminderThree % 2;
-			// 	optionOne = Math.floor(reminderTwo / 1);
-			// 	bestPrice = (threePizzasPrice * optionThree) + (twoPizzasPrice * optionTwo) + (onePizzaPrice * optionOne);
-			// //Display the best price for enetered number of pizzas
-			// 	document.getElementById("message").textContent = `$${bestPrice}`;
+			switch (radioBtnValue){
+				case radioBtnValue = "Hungry":
+
+
+				case radioBtnValue = "Extra hungry":
+
+			}
+			
+			//Calculate the best option for entered number of pizzas
+				optionThree = Math.floor(numOfPizzas / 3);
+				reminderThree = numOfPizzas % 3;
+				optionTwo = Math.floor(reminderThree / 2);
+				reminderTwo = reminderThree % 2;
+				optionOne = Math.floor(reminderTwo / 1);
+				bestPrice = (threePizzasPrice * optionThree) + (twoPizzasPrice * optionTwo) + (onePizzaPrice * optionOne);
+			//Display the best price for enetered number of pizzas
+				document.getElementById("message").textContent = `$${bestPrice}`;
 			}
 	});
 	
