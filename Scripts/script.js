4fe28082dd09042ef45c8feaec9746e1beb15eac
Monika <mprzybylska601@gmail.com>
2023-05-15 14:41:38 +1000

    Started implementing JS logic

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,33 +7,40 @@
 * @author Monika Przybylska
 */
 //function checkNumberOfPeople(){
-	let recommendedNumber;
 	let calcBtn = document.getElementById("calcBtn");
 	let radioBtn = document.getElementsByName("hungryValue");
 	
 	calcBtn.addEventListener('click', ()=>{
 		let  radioBtnValue;
-		let numOfPizzas= document.getElementById("quantity").value;
+		let smallPizza = 6;
+		let mediumPizza = 8;
+		let largePizza = 10;
+		let recommendedNumber;
+		let numOfPeople= document.getElementById("quantity").value;
 		for(let i = 0; i< radioBtn.length; i++){
 			if(radioBtn[i].checked){
 				radioBtnValue = radioBtn[i].labels[0].innerText;	
 			}
 		}
-		if (numOfPizzas == "" || isNaN(numOfPizzas) || numOfPizzas %  1 != 0 || numOfPizzas <= 0 || radioBtnValue == "undefined"){
+		if (numOfPeople == "" || isNaN(numOfPeople) || numOfPeople %  1 != 0 || numOfPeople <= 0 || radioBtnValue == "undefined"){
 			document.getElementById("message").textContent = `Please provide a number and choose how hungry you are`;
 		}
 		else{
+			let totalSlices;
 			switch (radioBtnValue){
 				case radioBtnValue = "Hungry":
-
-
+					totalSlices = numOfPeople * 5;
+				//5 slices of pizza for hungry
+				
 				case radioBtnValue = "Extra hungry":
+				//6 slices ofpizza for extra hungry
+					totalSlices = numOfPeople * 6;
 
 			}
-			
+			 
 			//Calculate the best option for entered number of pizzas
-				optionThree = Math.floor(numOfPizzas / 3);
-				reminderThree = numOfPizzas % 3;
+				optionThree = Math.floor(numOfPeople / 3);
+				reminderThree = numOfPeople % 3;
 				optionTwo = Math.floor(reminderThree / 2);
 				reminderTwo = reminderThree % 2;
 				optionOne = Math.floor(reminderTwo / 1);
